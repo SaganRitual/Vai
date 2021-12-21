4a77e7341380c145b01a34e553dfc0d08021ea8e
Rob Bishop <c.sagan.ritual@gmail.com>
2021-12-21 13:00:37 -0800

    Initial Commit

---
+++ Shared/VaiApp.swift
@@ -0,0 +1,12 @@
+// We are a way for the cosmos to know itself. -- C. Sagan
+
+import SwiftUI
+
+@main
+struct VaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
